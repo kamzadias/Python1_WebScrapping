4bd65836e1ae5f49f80fbe8d9a71a451b27c7ec1
Dias Kamza <68639981+kamzadias@users.noreply.github.com>
2021-10-10 22:49:26 +0600

    Update Assigment2.py

--- src/Assigment2.py
+++ src/Assigment2.py
@@ -5,7 +5,7 @@ class Scrapper:
    def get(self, coin):
       r = requests.get("https://www.google.com/search?q=site%3Acoinmarketcap.com+"+coin).text
       
-		  #r2=requests.get("https://coinmarketcap.com/currencies/'+coin+'/news/").text
+      #r2=requests.get("https://coinmarketcap.com/currencies/'+coin+'/news/").text
       #If needed to get the paragpaphs from the coinmarketcap website itself, then use this link
 
       soup = BeautifulSoup(r, 'html.parser')
